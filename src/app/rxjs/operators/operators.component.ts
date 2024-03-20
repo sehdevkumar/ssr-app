fd31f708cc0561fe74453d37a68e9bf69a61c6f4
sehdev <sehdev@atai.ai>
2024-03-20 12:03:49 +0530

    some changes

--- src/app/rxjs/operators/operators.component.ts
+++ src/app/rxjs/operators/operators.component.ts
@@ -1,10 +1,11 @@
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { CommonModule } from '@angular/common'
 import { HttpClient, HttpClientModule } from '@angular/common/http'
-import { Component, OnInit, inject } from '@angular/core'
+import { Component, OnInit, afterRender, inject } from '@angular/core'
 import {
   BehaviorSubject,
   Subject,
+  combineLatest,
   concatMap,
   delay,
   from,
@@ -19,6 +20,36 @@ import {
 import { MatOptionModule } from '@angular/material/core'
 import { MatInputModule } from '@angular/material/input'
 import { MatSelectModule } from '@angular/material/select'
+
+/**
+ * @fileoverview
+ *
+ * To delve into a more extensive list of commonly used RxJS operators, you can explore the following operators:
+
+1. **map**: Transforms each value emitted by the Observable.
+2. **filter**: Filters the values emitted by the Observable based on a predicate.
+3. **merge**: Merges multiple Observables into one.
+4. **combineLatest**: Combines the latest values from multiple Observables.
+5. **switchMap**: Projects each source value to an Observable, then flattens these into a single Observable.
+6. **concat**: Concatenates multiple Observables together by sequentially emitting their values.
+7. **take**: Emits only the first `n` values emitted by an Observable.
+8. **debounceTime**: Emits a value from the source Observable only after a specified time has passed without another source emission.
+9. **distinctUntilChanged**: Emits all values emitted by the source Observable that are distinct from the previous value.
+10. **scan**: Applies an accumulator function over the source Observable, emitting each intermediate result.
+11. **startWith**: Emits a specified value as the first emission before the source Observable begins emitting.
+12. **pluck**: Selects a specific property from each source value.
+13. **retry**: Re-subscribes to the source Observable a specified number of times if an error occurs.
+14. **catchError**: Handles errors in the source Observable by returning a new Observable or throwing an error.
+15. **tap**: Intercepts each emission on the source Observable for side effects.
+16. **finalize**: Invokes a specified function when the source Observable completes or errors.
+17. **distinctUntilKeyChanged**: Emits all values emitted by the source Observable that are distinct based on a specific key.
+18. **takeUntil**: Emits values from the source Observable until a second Observable emits.
+19. **delay**: Delays the emissions from the source Observable by a specified amount of time.
+20. **shareReplay**: Shares a single subscription to the source Observable and replays a specified number of emissions to subsequent subscribers.
+
+These operators are commonly used in RxJS programming to manipulate, filter, combine, and control the flow of data streams effectively.
+ *
+ */
 @Component({
   selector: 'app-operators',
   standalone: true,
@@ -37,17 +68,30 @@ export class OperatorsComponent implements OnInit {
   posts: any
   sub: BehaviorSubject<any> = new BehaviorSubject('')
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    afterRender(() => {
+      this.combineLatestFun()
+    })
+  }
 
   getData(param) {
     return this.http.get(`https://jsonplaceholder.typicode.com/posts/${param}`)
   }
 
-  ngOnInit(): void {
-    this.concatMapFun()
+  ngOnInit(): void {}
+
+  // ===============================CombineLatest==========================================
+
+  combineLatestFun() {
+    const obs1 = timer(5000).pipe(map(() => 'I am main'))
+    const obs2 = interval(600).pipe(map(() => Math.random()),delay(2000))
+
+    combineLatest([obs1, obs2]).subscribe((res) => {
+      // console.log(res)
+    })
   }
 
-   // ============================ConcatMap====================================
+  // ============================ConcatMap====================================
   // ! concatMap use to concat the nested sunscriptions but do  have the  order in which values emitted.
   concatMapFun() {
     from([1, 2, 3, 4, 5])
